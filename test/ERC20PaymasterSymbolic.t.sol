69f4f19ed599ec69daab6c9dab6dfdc93a296c47
Kristof Gazso <kristof.gazso@gmail.com>
2024-03-08 01:36:07 +0000

    remove broken test

--- test/ERC20PaymasterSymbolic.t.sol
+++ test/ERC20PaymasterSymbolic.t.sol
@@ -95,43 +95,4 @@ contract ERC20PaymasterSymbolicTest is SymTest, Test {
             assert(priceMarkup <= priceMarkupLimit);
         }
     }
-
-    function check_postOp_neverUnderpaid() public {
-        vm.deal(address(account), 1e18);
-        token.sudoMint(address(account), 1000e18); // 1000 usdc;
-        token.sudoMint(address(paymaster), 1); // 1000 usdc;
-        token.sudoApprove(address(account), address(paymaster), 1000e18);
-        PackedUserOperation memory op =
-            fillUserOp(account, userKey, address(counter), 0, abi.encodeWithSelector(TestCounter.count.selector));
-        // op.paymasterAndData = abi.encodePacked(address(paymaster), uint128(100000), uint128(100000));
-        // op.signature = signUserOp(op, userKey);
-        // submitUserOp(op);
-    }
-
-    function fillUserOp(SimpleAccount _sender, uint256 _key, address _to, uint256 _value, bytes memory _data)
-        public
-        view
-        returns (PackedUserOperation memory op)
-    {
-        op.sender = address(_sender);
-        op.nonce = entryPoint.getNonce(address(_sender), 0);
-        op.callData = abi.encodeWithSelector(SimpleAccount.execute.selector, _to, _value, _data);
-        op.accountGasLimits = bytes32(abi.encodePacked(bytes16(uint128(80000)), bytes16(uint128(50000))));
-        op.preVerificationGas = 50000;
-        op.gasFees = bytes32(abi.encodePacked(bytes16(uint128(100)), bytes16(uint128(1000000000))));
-        op.signature = signUserOp(op, _key);
-        return op;
-    }
-
-    function signUserOp(PackedUserOperation memory op, uint256 _key) public view returns (bytes memory signature) {
-        bytes32 hash = entryPoint.getUserOpHash(op);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_key, MessageHashUtils.toEthSignedMessageHash(hash));
-        signature = abi.encodePacked(r, s, v);
-    }
-
-    function submitUserOp(PackedUserOperation memory op) public {
-        PackedUserOperation[] memory ops = new PackedUserOperation[](1);
-        ops[0] = op;
-        entryPoint.handleOps(ops, beneficiary);
-    }
 }
