9d8bbcbef4ade2b7b5b61013b8feff51553c50ae
pavlovdog <pavlovdog.moscow@gmail.com>
2024-04-13 06:21:50 +0400

    Increase tests coverage
    - Add forking mode

--- src/TwapOracle.sol
+++ src/TwapOracle.sol
@@ -16,9 +16,18 @@ contract TwapOracle is IOracle, Ownable {
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
     error InvalidTwapAge();
 
+    error InvalidTokenOrPool();
+
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                  CONSTANTS AND IMMUTABLES                  */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    /// @dev Fixed price mode, a price is always equal to 1e8
+    /// @dev Used for the case when there's token/native coin
+    /// @dev but no token/usdc pool available
+    /// @dev The `nativeAssetOracle` would be a TWAP oracle for token/native coin pool
+    /// @dev The `tokenOracle` would TWAP oracle in fixed price mode
+    bool public immutable fixedPriceMode;
+
     /// @dev The Uniswap V3 pool address
     address public immutable pool;
 
@@ -26,13 +35,13 @@ contract TwapOracle is IOracle, Ownable {
     address public immutable baseToken;
 
     /// @dev The base token decimals
-    uint8 public immutable baseTokenDecimals;
+    uint256 public immutable baseTokenDecimals;
 
     /// @dev The quote token address (WETH or USD stable coin)
     address public immutable quoteToken;
 
     /// @dev The quote token decimals
-    uint8 public immutable quoteTokenDecimals;
+    uint256 public immutable quoteTokenDecimals;
 
     /// @dev Default TWAP age, used to fetch the price
     uint32 public twapAge;
@@ -40,22 +49,29 @@ contract TwapOracle is IOracle, Ownable {
     uint32 public constant MINIMUM_TWAP_AGE = 1 minutes;
     uint32 public constant MAXIMUM_TWAP_AGE = 7 days;
 
+    uint256 public constant ORACLE_DECIMALS = 1e8;
+
     constructor(
+        bool _fixedPriceMode,
         address _pool,
         uint32 _twapAge,
         address _baseToken,
         address _owner
     ) Ownable(_owner) {
+        fixedPriceMode = _fixedPriceMode;
         pool = _pool;
-        twapAge = _twapAge;
+        _setTwapAge(_twapAge);
+
+        address token0 = IUniswapV3PoolImmutables(_pool).token0();
+        address token1 = IUniswapV3PoolImmutables(_pool).token1();
+
+        if (_baseToken != token0 && _baseToken != token1) revert InvalidTokenOrPool();
 
         baseToken = _baseToken;
-        baseTokenDecimals = IERC20Metadata(baseToken).decimals();
+        baseTokenDecimals = 10 ** IERC20Metadata(baseToken).decimals();
 
-        quoteToken = IUniswapV3PoolImmutables(_pool).token0() == baseToken ?
-            IUniswapV3PoolImmutables(_pool).token1() :
-            IUniswapV3PoolImmutables(_pool).token0();
-        quoteTokenDecimals = IERC20Metadata(quoteToken).decimals();
+        quoteToken = token0 == baseToken ? token1 : token0;
+        quoteTokenDecimals = 10 ** IERC20Metadata(quoteToken).decimals();
     }
 
     function decimals() external override pure returns (uint8) {
@@ -65,9 +81,11 @@ contract TwapOracle is IOracle, Ownable {
     function setTwapAge(
         uint32 _twapAge
     ) public onlyOwner() {
-        if (_twapAge < MINIMUM_TWAP_AGE || _twapAge > MAXIMUM_TWAP_AGE) {
-            revert InvalidTwapAge();
-        }
+        _setTwapAge(_twapAge);
+    }
+
+    function _setTwapAge(uint32 _twapAge) internal {
+        if (_twapAge < MINIMUM_TWAP_AGE || _twapAge > MAXIMUM_TWAP_AGE) revert InvalidTwapAge();
 
         twapAge = _twapAge;
     }
@@ -79,24 +97,32 @@ contract TwapOracle is IOracle, Ownable {
         uint256 updatedAt,
         uint80 answeredInRound
     ) {
+        if (fixedPriceMode) return _buildLatestRoundData(ORACLE_DECIMALS);
+
         uint256 _price = _fetchTwap();
-        console.log("_price: %d", _price);
 
-        // Convert the price to 8 decimals
-        uint256 price = _price * (10 ** 8) / (10 ** quoteTokenDecimals);
+        // Normalize the price to the oracle decimals
+        uint256 price = _price * ORACLE_DECIMALS / quoteTokenDecimals;
 
+        return _buildLatestRoundData(price);
+    }
+
+    function _buildLatestRoundData(uint256 price) internal view returns (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound
+    ) {
         return (0, int256(price), 0, block.timestamp - 1, 0);
     }
 
     function _fetchTwap() internal view returns (uint256) {
         (int24 arithmeticMeanTick,) = OracleLibrary.consult(pool, twapAge);
 
-        // Base token amount is equal to 1
-        uint128 baseAmount = uint128(10 ** baseTokenDecimals);
-
         return OracleLibrary.getQuoteAtTick(
             arithmeticMeanTick,
-            baseAmount,
+            uint128(baseTokenDecimals), // Base token amount is equal to 1 token
             baseToken,
             quoteToken
         );
--- test/TwapOracle.t.sol
+++ test/TwapOracle.t.sol
@@ -2,64 +2,49 @@
 pragma solidity ^0.8.0;
 
 import "src/TwapOracle.sol";
-import "./utils/TestUniswapV3Pool.sol";
-import "./utils/TestERC20.sol";
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
+import {IUniswapV3Pool} from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
 contract TwapOracleTest is Test {
     address owner;
 
-    TestERC20 baseToken;
-    TestERC20 quoteToken;
-    TestUniswapV3Pool pool;
-    TwapOracle oracle;
-
     function setUp() external {
         owner = makeAddr("owner");
-
-        baseToken = new TestERC20(18);
-        quoteToken = new TestERC20(18);
-
-        // 0xc9034c3e7f58003e6ae0c8438e7c8f4598d5acaa
-        // weth/degen
-        // base, block 13065501
-        pool = new TestUniswapV3Pool(
-            address(baseToken),
-            address(quoteToken),
-            [
-                int56(1187440464978),
-                int56(1187856501264)
-            ],
-            [
-                uint160(6047764207030777331073),
-                uint160(6048299056841155705324)
-            ]
-        );
-
-        oracle = new TwapOracle(
-            address(pool),
-            1 hours,
-            address(baseToken),
-            owner
-        );
     }
 
-    function testLatestRoundData() external {
-        (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        ) = oracle.latestRoundData();
+    function testWbtcUsdt() external {
+        IUniswapV3Pool pool = IUniswapV3Pool(0x9Db9e0e53058C89e5B94e29621a205198648425B);
 
-        assertEq(roundId, 0);
-        assertEq(startedAt, 0);
-        assertEq(answeredInRound, 0);
+        TwapOracle oracle = new TwapOracle(
+            false,
+            address(pool),
+            1 hours,
+            0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
+            owner
+        );
+
+        (,int256 answer,,uint256 updatedAt,) = oracle.latestRoundData();
+
         assertEq(updatedAt, block.timestamp - 1);
+        assertApproxEqAbs(answer / 10**8, 60000, 10000, "Wrong BTC/USDT price");
+    }
 
-        console.log("answer: %d", uint256(answer));
+    function testUsdcWeth() external {
+        IUniswapV3Pool pool = IUniswapV3Pool(0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640);
+
+        TwapOracle oracle = new TwapOracle(
+            false,
+            address(pool),
+            1 hours,
+            0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
+            owner
+        );
+
+        (,int256 answer,,,) = oracle.latestRoundData();
+ 
+        assertApproxEqAbs(answer / 10**8, 30000, 5000, "Wrong USDC/WETH price");
     }
 }
